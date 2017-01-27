0d93edea31b9c563f5a1b914671bdc4e32f53c1d
rgupta1992 <mail.rgupta1992@gmail.com>
2017-01-27 19:24:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./'));
 
 app.use('/', function (req, res) {
-    res.sendFile(path.resolve('client/index.html'));
+    res.sendFile(path.resolve('index.html'));
 });
 
-var port = 3000;
+var port = 8080;
 
 app.listen(port, function(error) {
   if (error) throw error;
   console.log("Express server listening on port", port);
 });
+
+
+
+
+
+
+
